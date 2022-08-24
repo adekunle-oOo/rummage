3a7d1c1e7ff8a2b0707c44ad73f30b4fff13dd2c
adekunle-oOo <adekunleoludele@outlook.com>
2022-08-24 20:53:55 +0530

    added versioning

--- misc.go
+++ misc.go
@@ -16,7 +16,7 @@ import (
 	"github.com/wealdtech/go-multicodec"
 )
 
-//defining the global variables used
+// defining the global variables used
 var (
 	Shell        *ipfsapi.Shell
 	Client       *ethclient.Client
@@ -25,9 +25,10 @@ var (
 	serverIP     string
 	passWord     string
 	latestHLICid string
+	version      uint
 )
 
-//function to convert to base32 encoding
+// function to convert to base32 encoding
 func b32Cid(bytes []byte) (string, error) {
 	data, codec, err := multicodec.RemoveCodec(bytes)
 	if err != nil {
@@ -51,40 +52,39 @@ func b32Cid(bytes []byte) (string, error) {
 	}
 
 	return "", fmt.Errorf("unknown codec name %s", codecName)
-
 }
 
-//simple way of ensuring we have pdf's
-//this will become more sophisticated in future releases
+// simple way of ensuring we have pdf's
+// this will become more sophisticated in future releases
 func isValidPdf(stream []byte) bool {
-	//fmt.Println(stream)
+	// fmt.Println(stream)
 
 	l := len(stream)
-	var isHeaderValid = stream[0] == 0x25 && stream[1] == 0x50 && stream[2] == 0x44 && stream[3] == 0x46 //%PDF
+	isHeaderValid := stream[0] == 0x25 && stream[1] == 0x50 && stream[2] == 0x44 && stream[3] == 0x46 //%PDF
 
 	//(.%%EOF)
-	var isTrailerValid1 = stream[l-6] == 0xa && stream[l-5] == 0x25 && stream[l-4] == 0x25 &&
+	isTrailerValid1 := stream[l-6] == 0xa && stream[l-5] == 0x25 && stream[l-4] == 0x25 &&
 		stream[l-3] == 0x45 && stream[l-2] == 0x4f && stream[l-1] == 0x46
 	if isHeaderValid && isTrailerValid1 {
 		return true
 	}
 
 	//(.%%EOF.)
-	var isTrailerValid2 = stream[l-7] == 0xa && stream[l-6] == 0x25 && stream[l-5] == 0x25 && stream[l-4] == 0x45 &&
+	isTrailerValid2 := stream[l-7] == 0xa && stream[l-6] == 0x25 && stream[l-5] == 0x25 && stream[l-4] == 0x45 &&
 		stream[l-3] == 0x4f && stream[l-2] == 0x46 && stream[l-1] == 0xa
 	if isHeaderValid && isTrailerValid2 {
 		return true
 	}
 
 	//(.%%EOF.)
-	var isTrailerValid4 = stream[l-7] == 0xd && stream[l-6] == 0x25 && stream[l-5] == 0x25 && stream[l-4] == 0x45 &&
+	isTrailerValid4 := stream[l-7] == 0xd && stream[l-6] == 0x25 && stream[l-5] == 0x25 && stream[l-4] == 0x45 &&
 		stream[l-3] == 0x4f && stream[l-2] == 0x46 && stream[l-1] == 0xd
 	if isHeaderValid && isTrailerValid4 {
 		return true
 	}
 
 	//(..%%EOF..)
-	var isTrailerValid3 = stream[l-8] == 0xd && stream[l-7] == 0x25 && stream[l-6] == 0x25 && stream[l-5] == 0x45 && stream[l-4] == 0x4f &&
+	isTrailerValid3 := stream[l-8] == 0xd && stream[l-7] == 0x25 && stream[l-6] == 0x25 && stream[l-5] == 0x45 && stream[l-4] == 0x4f &&
 		stream[l-3] == 0x46 && stream[l-2] == 0xd && stream[l-1] == 0xa
 	if isHeaderValid && isTrailerValid3 {
 		return true
@@ -93,7 +93,7 @@ func isValidPdf(stream []byte) bool {
 	return false
 }
 
-//function to get the latest ipns record from the server for the HLI
+// function to get the latest ipns record from the server for the HLI
 func getHLI() (string, error) {
 	addr := strings.Join([]string{serverIP, strconv.Itoa(serverPort)}, ":")
 	conn, err := net.Dial("tcp", addr)
@@ -111,11 +111,11 @@ func getHLI() (string, error) {
 	n, _ := conn.Read(buff)
 	data := string(buff[:n])
 	HLI := strings.Trim(data, "\r\n\r\n")
-	//log.Printf("Update HLI: %s",HLI)
+	// log.Printf("Update HLI: %s",HLI)
 	return HLI, nil
 }
 
-//checks if required directories are present, if not create them
+// checks if required directories are present, if not create them
 func setDirectories() error {
 	if _, err := os.Stat("./HLI"); os.IsNotExist(err) {
 		err := os.Mkdir("./HLI", 0700)
@@ -144,7 +144,7 @@ func setDirectories() error {
 	return nil
 }
 
-//starts pinning new HLI cid and removes old entry
+// starts pinning new HLI cid and removes old entry
 func updatePin(newHLICid string) {
 	err := Shell.Pin(newHLICid)
 	err = Shell.Unpin(latestHLICid)
